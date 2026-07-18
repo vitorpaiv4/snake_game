f9735b73a01a6806848f3ce377e019c6a5e55880
vitorpaiv4 <paivavitorr@gmail.com>
2026-07-18 16:00:13 -0400

    Fix: Adcionando a cor ao fundo da tela e definincido a posição e estrutura da cobra

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use macroquad::prelude::*;
 
+// Criando estrutura
+struct Ponto {
+    x: f32,
+    y: f32,
+}
 
 #[macroquad::main("Snake Game")]
 async fn main() {
     loop {
-        //limpa a tela 
+       // Definimos a posição inicial da cabeça da cobra
+       let mut cabeca = Ponto {x: 10.0, y:10.0};
+
+       //tamanho inicial de cada bloco de pixel
+       let tamanho_bloco = 20.0;
+
+       loop{
         clear_background(DARKGRAY);
 
-        //Aguarda o próximo frame
+        //DESENHANDO O RETANGULO (CABEÇA DA COBRA)
+        draw_rectangle(
+            cabeca.x * tamanho_bloco,
+            cabeca.y * tamanho_bloco, 
+            tamanho_bloco,
+            tamanho_bloco,
+            GREEN
+        
+        );
+
         next_frame().await;
+       }
+    
     }
 }
