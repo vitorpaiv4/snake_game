8af0a4a3d3186e2fc33f25a50053bbd4cd838efd
vitorpaiv4 <paivavitorr@gmail.com>
2026-07-18 16:08:20 -0400

    Fix: Dando movimentação a cabeça

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,26 @@ async fn main() {
     loop {
        // Definimos a posição inicial da cabeça da cobra
        let mut cabeca = Ponto {x: 10.0, y:10.0};
-
        //tamanho inicial de cada bloco de pixel
        let tamanho_bloco = 20.0;
 
        loop{
         clear_background(DARKGRAY);
+
+        //Lendo o teclado para movimentar a cobra   
+        if is_key_pressed(KeyCode::Right) {
+            cabeca.x += 1.0;
+        }else if is_key_pressed(KeyCode::Left) {
+            cabeca.x -= 1.0 ;
+        }else if is_key_pressed(KeyCode::Down) {
+            cabeca.y += 1.0;
+        }
+        else if is_key_pressed(KeyCode::Up) {
+            cabeca.y -= 1.0;
+        }
+    
+
+
 
         //DESENHANDO O RETANGULO (CABEÇA DA COBRA)
         draw_rectangle(
