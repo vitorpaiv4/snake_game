fe8aeb89c87f124c9b1e6a4a595086bf2504ef21
vitorpaiv4 <paivavitorr@gmail.com>
2026-07-18 15:51:05 -0400

    Fix: Adcionando a primeita tela e o limpador de tela

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+
+#[macroquad::main("Snake Game")]
+async fn main() {
+    loop {
+        //limpa a tela 
+        clear_background(DARKGRAY);
+
+        //Aguarda o próximo frame
+        next_frame().await;
+    }
 }
